78a45c5ce00a153e8312765c9d59e759b74993c1
BAIN\39847 <saopazo@uc.cl>
2017-06-20 20:17:04 -0300

    Poblar DB Countries...desde CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'countries.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'UTF-8')
+csv.each do |row|
+  t = Country.new
+  t.name = row['name']
+  t.code = row['code']
+  t.save
+  puts "#{t.name}, #{t.code} saved"
+end
+
+
+
+
 
 { pis_confins: 0.04, interest_2_30: 0.01, interest_31_60: 0.02, interest_more_60: 0.03 }.each do |name, value|
   sys_var = SystemVariable.find_or_create_by(name: name)
