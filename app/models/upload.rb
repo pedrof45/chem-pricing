b87e6c504d0656b9cb2f198d221127533e990f98
saopazo <saopazo@uc.cl>
2017-07-05 19:11:04 -0300

    sales = sale

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -7,7 +7,7 @@ class Upload < ApplicationRecord
 
   validates_presence_of :model, :file, :user
 
-  enumerize :model, in: [:quote,:optimal_markup, :sales, :cost, :customer, :product, :icms_tax, :city,
+  enumerize :model, in: [:quote,:optimal_markup, :sale, :cost, :customer, :product, :icms_tax, :city,
                          :normal_bulk_freight, :chopped_bulk_freight, :product_bulk_freight, :especial_packed_freight, :normal_packed_freight]
 
   def parse
