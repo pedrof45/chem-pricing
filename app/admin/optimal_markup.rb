c0810c26e07ba48ae56152dbd2277f849d0723d4
saopazo <saopazo@uc.cl>
2017-07-04 23:10:55 -0300

    no message

--- app/admin/optimal_markup.rb
+++ app/admin/optimal_markup.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register OptimalMarkup do
   menu label: '2. Markups', priority: 3
-  permit_params :customer_id, :product_id, :dist_center_id, :value, :business_unit_id, :table_value, :metodology
+  permit_params :metodology, :customer_id, :product_id, :dist_center_id, :value, :business_unit_id, :table_value
   actions :all
 
   index title: 'Markups por Cliente e Produto' do
