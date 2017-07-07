ce803ac119d09222baab75d1657b3a773702f604
saopazo <saopazo@uc.cl>
2017-07-07 16:15:35 -0300

    si es fob no requiere subtipo

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -68,7 +68,7 @@ class Quote < ApplicationRecord
     unless errors.any?
       freight_service.run
     end
-    unless errors.any? || unit_freight.nil?
+    unless errors.any? || unit_freight.nil? || unit_freight == 0
       simulator_service.run
     end
   end
