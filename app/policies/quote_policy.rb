67c121d14101deba2542fe0c82e042dbdea5be1a
saopazo <saopazo@uc.cl>
2017-07-10 19:17:34 -0300

    Pundit para users y para quotes

--- app/policies/quote_policy.rb
+++ app/policies/quote_policy.rb
@@ -1,9 +1,30 @@
 class QuotePolicy < ApplicationPolicy
+	class Scope < Scope
+	    attr_reader :user, :scope
 
+	    def initialize(user,scope)
+	      @user =user
+	      @scope =scope
+	      
+	    end
+
+	    def resolve
+	      if user.sysadmin_or_more?
+	        scope.all
+	      else
+	        scope.where(:user_id => user.id)
+	      end
+	    end
+	  end
 
 	def index?
-		#FIX
-	  @quotes = @user.quotes
-	end
+
+      if user.manager_or_more?
+        true
+      else
+        @quotes = UserPolicy::Scope.new(@user,Quote).resolve
+      end
+    
+    end
 
 end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,31 +1,53 @@
 class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    attr_reader :user, :scope
 
-  def index?
-  scope.where(:id => record.id=user.id)
+    def initialize(user,scope)
+      @user =user
+      @scope =scope
+      
+    end
+
+    def resolve
+      if user.sysadmin_or_more?
+        scope.all
+      else
+        scope.where(:id => user.id)
+      end
+    end
   end
 
-  def update?
-	 if record.sysadmin_or_more?
-		user.sysadmin_or_more?
-	elsif record.admin_or_more?
-		sysadmin_or_more_or_self
-	elsif record.manager_or_more?
-		sysadmin_or_more_or_self
-	else 
-		sysadmin_or_more_or_self
-	end
-end
+    def index?
 
-  def create?
-    user.sysadmin_or_more?
-  end
+      if user.sysadmin_or_more?
+        true
+      else
+        @users = UserPolicy::Scope.new(@user,User).resolve
+      end
+    
+    end
 
-  def destroy?
-    user.sysadmin_or_more?
-  end
+    def update?
+  	 if record.sysadmin_or_more?
+  		user.sysadmin_or_more?
+  	elsif record.admin_or_more?
+  		sysadmin_or_more_or_self
+  	elsif record.manager_or_more?
+  		sysadmin_or_more_or_self
+  	else 
+  		sysadmin_or_more_or_self
+  	end
+   end
 
-   def show?
-    user.sysadmin_or_more?
-  end
+    def create?
+      user.sysadmin_or_more?
+    end
 
+    def destroy?
+      user.sysadmin_or_more?
+    end
+
+     def show?
+      user.sysadmin_or_more?
+    end
 end
