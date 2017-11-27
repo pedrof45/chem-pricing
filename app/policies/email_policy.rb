422a1bcc56420be1e01ad776b1310b1d33a63436
felbalart <felbalart@gmail.com>
2017-11-27 02:26:30 -0300

    feat(emails): set scope on policy

--- app/policies/email_policy.rb
+++ app/policies/email_policy.rb
@@ -1,2 +1,18 @@
 class EmailPolicy < ApplicationPolicy
+  class Scope < Scope
+    attr_reader :user, :scope
+
+    def initialize(user,scope)
+      @user =user
+      @scope =scope
+    end
+
+    def resolve
+      if user.manager_or_more?
+        scope.all
+      else
+        scope.where(:user_id => user.id)
+      end
+    end
+  end
 end
