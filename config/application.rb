96fcc17a699f26c2f5ad07d4d78f569c43960970
felbalart <felbalart@gmail.com>
2017-11-27 02:42:08 -0300

    fix(app): change default email url env var

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module ChemPricing
     config.load_defaults 5.1
     config.time_zone = 'Brasilia'
     config.active_record.default_timezone = :utc
-    config.action_mailer.default_url_options = { host: ENV["SMTP_DOMAIN"] || ENV['APPLICATION_HOST'] }
+    config.action_mailer.default_url_options = { host: ENV['APPLICATION_HOST'] }
     config.action_mailer.smtp_settings = {
         address: ENV["SMTP_ADDRESS"],
         authentication: :plain,
