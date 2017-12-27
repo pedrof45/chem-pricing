21e400081e1a16268e1d3459c553f7dffc0c2c7f
felbalart <felbalart@gmail.com>
2017-12-27 14:43:43 -0300

    feat(mailer): adds 'from' user email

--- app/mailers/quote_mailer.rb
+++ app/mailers/quote_mailer.rb
@@ -2,7 +2,7 @@ class QuoteMailer < ApplicationMailer
 
   def send_email(email)
     @email = email
-    mail(to: email.recipient, subject: email.subject) do |format|
+    mail(from: email.user.email, to: email.recipient, subject: email.subject) do |format|
       format.html { render 'quotes_mailer/quotes_mail' }
     end
   end
--- config/application.rb
+++ config/application.rb
@@ -24,15 +24,15 @@ module ChemPricing
     config.action_controller.asset_host = ENV['APPLICATION_HOST']
     config.action_mailer.default_url_options = { host: ENV['APPLICATION_HOST'] }
     config.action_mailer.asset_host = ENV['MAILER_HOST'] || ENV['APPLICATION_HOST']
-    config.action_mailer.smtp_settings = {
-        address: ENV["SMTP_ADDRESS"],
-        authentication: :plain,
-        domain: ENV["SMTP_DOMAIN"],
-        enable_starttls_auto: true,
-        password: ENV["SMTP_PASSWORD"],
-        port: ENV["SMTP_PORT"],
-        user_name: ENV["SMTP_USERNAME"]
-    }
+    # config.action_mailer.smtp_settings = {
+    #     address: ENV["SMTP_ADDRESS"],
+    #     authentication: :plain,
+    #     domain: ENV["SMTP_DOMAIN"],
+    #     enable_starttls_auto: true,
+    #     password: ENV["SMTP_PASSWORD"],
+    #     port: ENV["SMTP_PORT"],
+    #     user_name: ENV["SMTP_USERNAME"]
+    # }
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
   end
